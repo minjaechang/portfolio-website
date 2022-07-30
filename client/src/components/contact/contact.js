9ffd7c04c8d23c866ea5a900bdd57489ac78b351
Minjae Chang <minjaejchang@gmail.com>
2022-07-31 00:09:29 +1200

    Fix logo issue

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,22 +1,22 @@
 import styles from './contact.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = (props) => (
   <section id='contact' className={styles.contact}>
     <div className={styles.contact__text}>
-      <h4 className={styles.logo}>minjae</h4>
+      <h4 className='logo'>minjae</h4>
       <p className={styles.contact__email}>minjae.j.chang@gmail.com</p>
       <p className={styles.contact__location}>Auckland, New Zealand</p>
     </div>
     <div className={styles.contact__socialMedia}>
-      <a href=''>
-        <i className='fab fa-github'></i>
-      </a>
+      <FontAwesomeIcon icon={faCoffee} href='https://github.com/minjaechang' />
       <a href='https://www.linkedin.com/in/minjaechang/'>
         <i className='fab fa-linkedin'></i>
       </a>
-      <a href='/'>
+      {/* <a href='/'>
         <i className='fab fa-twitter'></i>
-      </a>
+      </a> */}
     </div>
   </section>
 );
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -3,7 +3,7 @@ import styles from './navbar.module.css';
 const Navbar = (props) => {
   return (
     <navbar className={styles.navbar}>
-      <h4 className={styles.logo}>minjae</h4>
+      <h4 className='logo'>minjae</h4>
       <ul className={styles.menu}>
         <li className={styles.item} data-link='#about'>
           About
